11221534d99b4cec13987e9fa878031fead3d541
Stephen Rivas <112515854+stephenscript@users.noreply.github.com>
2023-06-29 14:17:00 -0400

    useLogin Hook (#1)
    * completed useLogin custom hook and created baseline for LoginBox view to test useLogin hook. successfully pings backend and validates credentials
    
    * updated DNS settings in Vite to ensure receipt of cookies from backend
    
    * added documentation for useLogin hook

--- src/Hooks/useLogin.tsx
+++ src/Hooks/useLogin.tsx
@@ -1,5 +1,63 @@
-import React, { useState } from "react";
+/** useLogin.tsx
+ * input: username and password as strings
+ * output: isSuccess <null | boolean> representing login status
+ *
+ * hits /login endpoint of API to create an auth cookie used
+ * validate auth to interact with AI interface
+ *
+ * use: [isSuccess, message, setCredentials] = useLogin
+ * isSuccess: null if no attempt, false if failed, true if successful
+ * message: a string sent from the server, may indicate success or error
+ * setCredentials: function taking username and password strings as arguments and initiates login
+ **/
 
-export const useLogin = () => {
-    // code goes here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const api_url = import.meta.env.VITE_AI_API_URL;
+
+interface Credentials {
+  username: string;
+  password: string;
+}
+
+export const useLogin = (): [
+  boolean | null,
+  string,
+  React.Dispatch<React.SetStateAction<Credentials>>
+] => {
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+  const [message, setMessage] = useState("");
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    if (!credentials.username || !credentials.password) return;
+
+    axios
+      .post(`${api_url}/login`, JSON.stringify(credentials), {
+        withCredentials: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      })
+      .then((data) => {
+        setIsSuccess(true);
+        setMessage("Login Successful!");
+      })
+      .catch((error) => {
+        if (error.request.status === 500) {
+          setIsSuccess(false);
+          setMessage("Username and/or password incorrect. Please try again.");
+        } else {
+          setIsSuccess(false);
+          setMessage("Failed to connect to server.");
+        }
+      });
+  }, [credentials]);
+
+  return [isSuccess, message, setCredentials];
 };

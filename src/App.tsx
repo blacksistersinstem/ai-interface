ad1191a0f3254c09384ce0cf1f458dc01a55ca6e
Stephen <stephen.anthony.rivas@gmail.com>
2023-06-29 15:24:22 -0400

    implemented useAPI hook, which successfully pings back-end for AI response

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
 import { LoginBox } from "./Views/LoginBox/LoginBox";
-
+import { ResponseBox } from "./Views/ResponseBox/ResponseBox";
 
 const App = () => {
-
   return (
     <>
       <div>hello world</div>
       <LoginBox />
+      <ResponseBox />
     </>
   );
 };
--- src/Hooks/useAPI.tsx
+++ src/Hooks/useAPI.tsx
@@ -1,5 +1,57 @@
-import React, { useState } from "react";
+/** useAPI.tsx
+ * input: resume, currentRole, previousRole,
+ * output: isSuccess <null | boolean> representing login status
+ *
+ * hits /getResponse endpoint of API to create get response
+ * from AI query
+ *
+ * use: [isSuccess, response] = useAPI
+ * isSuccess: null if no attempt, false if failed, true if successful
+ * response: a data stream of text sent from the server
+ **/
 
-export const useAPI = () => {
-    // code goes here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const api_url = import.meta.env.VITE_AI_API_URL;
+
+export const useAPI = (
+  resume: Blob,
+  currentRole: string,
+  previousRole: string
+) => {
+  const [response, setResponse] = useState("");
+  const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (!resume || !currentRole || !previousRole) return;
+
+    axios
+      .post(
+        `${api_url}/getResponse`,
+        JSON.stringify({ resume, currentRole, previousRole }),
+        {
+          withCredentials: true,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((data) => {
+        setIsSuccess(true);
+        setResponse(data.data.response);
+      })
+      .catch((error) => {
+        if (error.request.status === 500) {
+          setIsSuccess(false);
+          setResponse(error.message);
+        } else {
+          setIsSuccess(false);
+          setResponse("Failed to connect to server.");
+        }
+      });
+  }, []);
+
+  return [isSuccess, response];
 };

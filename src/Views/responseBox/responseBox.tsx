891df97e3249c1135a1dee4ab4ac3a840cdd8933
Stephen Rivas <112515854+stephenscript@users.noreply.github.com>
2023-07-14 15:37:18 -0400

    Use API Hook (#4)
    * Implemented useAPI hook, which successfully pings back-end for AI response

--- src/Views/responseBox/responseBox.tsx
+++ src/Views/responseBox/responseBox.tsx
@@ -1,6 +1,19 @@
 // displays response from ai readable stream as text
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useAPI } from "../../Hooks/useAPI";
 
-export const responseBox = () => {
-    // code goes here
+export const ResponseBox = () => {
+  const [isSuccess, response] = useAPI(new Blob(), "test", "test");  
+
+  useEffect(() => {
+    //
+  }, [response]);
+
+  return (
+    <>
+      <div>Response</div>
+      <div>{response}</div>
+      {isSuccess && <div>{response}</div>}
+    </>
+  );
 };

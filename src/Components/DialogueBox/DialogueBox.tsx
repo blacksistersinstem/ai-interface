f319a68fdd23e748f8128e16025ab1f2722660ad
Stephen <stephen.anthony.rivas@gmail.com>
2023-07-23 17:59:46 -0400

    added padding for first question

---
+++ src/Components/DialogueBox/DialogueBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./DialogueBox.module.scss";
+
+export const DialogueBox = () => {
+  return (
+    <>
+      <section className={style.container}>
+        <div>hi</div>
+      </section>
+    </>
+  );
+};
